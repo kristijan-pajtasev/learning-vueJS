efc01548a06242ca7e07de636b293777a3d8b7a5
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2023-08-30 00:21:04 +0100

    values and method examples

--- part-2/script.js
+++ part-2/script.js
@@ -2,7 +2,15 @@
 Vue.createApp({
   data() {
     return {
-      message: "Hello world"
+      message: "Hello world",
+      arrayVariable: [1, 2, 3, 4, 5],
+      mapVariable: {firstName: "John", lastName: "Doe"},
+      nestedVariable: [{firstName: "John", lastName: "Doe"}, {firstName: "John", lastName: "Doe"}],
+      exampleName: "John",
     }
+  },
+  methods: {
+    getMessage: () => "Hello method",
+    getNameGreeting: () => `Hello ${this.exampleName}`
   }
 }).mount("#app")
