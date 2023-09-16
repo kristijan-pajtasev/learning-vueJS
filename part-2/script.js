15446ea8be4b4eecc96149631f58ffec8bc72e95
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2023-09-17 00:43:56 +0100

    link

--- part-2/script.js
+++ part-2/script.js
@@ -7,10 +7,11 @@ Vue.createApp({
       mapVariable: {firstName: "John", lastName: "Doe"},
       nestedVariable: [{firstName: "John", lastName: "Doe"}, {firstName: "John", lastName: "Doe"}],
       exampleName: "John",
+      linkValue: "https://www.google.com",
     }
   },
   methods: {
     getMessage: () => "Hello method",
-    getNameGreeting: () => `Hello ${this.exampleName}`
+    getNameGreeting: function(){ return `Hello ${this.exampleName}` }
   }
 }).mount("#app")
