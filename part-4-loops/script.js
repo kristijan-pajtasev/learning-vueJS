b48b5d7d82a0ea0d1b44db562a4f28d9801c0088
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2023-09-17 23:36:47 +0100

    init part 4

---
+++ part-4-loops/script.js
@@ -0,0 +1,29 @@
+// script.js
+Vue.createApp({
+  data() {
+    return {
+      people: [
+        {
+          id: 1,
+          firstName: "John",
+          lastName: "Doe",
+          dob: "01-01-1980"
+        },
+        {
+          id: 1,
+          firstName: "Johny",
+          lastName: "Doe",
+          dob: "01-01-1980"
+        },
+        {
+          id: 1,
+          firstName: "Jame",
+          lastName: "Doe",
+          dob: "01-01-1980"
+        }
+      ]
+    }
+  },
+  methods: {
+  }
+}).mount("#app")
