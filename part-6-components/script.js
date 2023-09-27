323c836244e8a6933c7bd2e964eafd4806c1f733
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2023-09-28 00:13:11 +0100

    components example

--- part-6-components/script.js
+++ part-6-components/script.js
@@ -1,8 +1,27 @@
 // script.js
-Vue.createApp({
+const PersonDetails = {
+  template: `
+    <div>
+      <div>{{firstName}}, {{lastName}}</div>
+    </div>`,
+  data() {
+    return {
+      firstName: this.$props.firstName,
+      lastName: this.$props.lastName
+    }
+  },
+  props: ["firstName", "lastName"],
+  methods: { }
+}
+
+const app = Vue.createApp({
   data() {
     return { }
   },
   methods: {
   }
-}).mount("#app")
+})
+
+app.component('PersonDetails', PersonDetails)
+
+app.mount("#app")
